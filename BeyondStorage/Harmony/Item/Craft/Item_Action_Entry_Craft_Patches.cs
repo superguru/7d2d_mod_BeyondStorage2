edc0446e426c4e09524baa69916ad22f03f4da4c
superguru <superguru@users.noreply.github.com>
2025-07-27 05:57:13 +1200

    Update Item_Action_Entry_Craft_Patches.cs
    Use new patching logic

--- BeyondStorage/Harmony/Item/Craft/Item_Action_Entry_Craft_Patches.cs
+++ BeyondStorage/Harmony/Item/Craft/Item_Action_Entry_Craft_Patches.cs
@@ -1,11 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Reflection.Emit;
 using BeyondStorage.Scripts.ContainerLogic.Item;
 using BeyondStorage.Scripts.Utils;
 using HarmonyLib;
-
 
 namespace BeyondStorage.Item.Action;
 
@@ -20,75 +18,36 @@ public class ItemActionEntryCraftPatches
     private static IEnumerable<CodeInstruction> XUiC_ItemStackGrid_HandleSlotChangedEvent_Patch(IEnumerable<CodeInstruction> instructions)
     {
         var targetMethodString = $"{typeof(ItemActionEntryCraft)}.{nameof(ItemActionEntryCraft.HasItems)}";
-        LogUtil.Info($"Transpiling {targetMethodString}");
 
-        var codes = new List<CodeInstruction>(instructions);
-
-        int patchIndex = 0;
-        int patchCount = 0;
-        int MAX_PATCHES = 1;
-
-        while ((patchIndex >= 0) && (patchIndex < codes.Count - 1))
+        // Create search pattern for GetAllItemStacks method call
+        var searchPattern = new List<CodeInstruction>
         {
-            if ((MAX_PATCHES > 0) && (patchCount >= MAX_PATCHES))
-            {
-                LogUtil.Info($"Reached maximum patches ({MAX_PATCHES}) for {targetMethodString}. Stopping further patches.");
-                break;
-            }
+            new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(XUiM_PlayerInventory), nameof(XUiM_PlayerInventory.GetAllItemStacks)))
+        };
 
-            ///* Original code snippet:
-            /// List<ItemStack> allItemStacks = xui.PlayerInventory.GetAllItemStacks();
-            //-02/* 0x0025B88E 03                 */ IL_0002: ldarg.1
-            //-01/* 0x0025B88F 7BB15C0004         */ IL_0003: ldfld class XUiM_PlayerInventory XUi::PlayerInventory
-            //+00/* 0x0025B894 6F19780006         */ IL_0008: callvirt instance class [mscorlib] System.Collections.Generic.List`1<class ItemStack> XUiM_PlayerInventory::GetAllItemStacks()
-            //+01/* 0x0025B899 0B                 */ IL_000d: stloc.1
-            //Count is 4, max index is 3
-
-            patchIndex = codes.FindIndex(patchIndex, code =>
-                code.opcode == OpCodes.Callvirt &&
-                code.operand is MethodInfo methodInfo
-                && methodInfo.Name == "GetAllItemStacks");
-
-            //patchIndex = codes.FindIndex(patchIndex, code => code.opcode == OpCodes.Ldfld && code.operand is FieldInfo fieldInfo && fieldInfo.Name == "isBurning");
-            //patchIndex = codes.FindIndex(patchIndex, code => code.opcode == OpCodes.Ldarg_0);
-            if (patchIndex < 0)
-            {
-                // No more matches found
-                break;
-            }
-
-            LogUtil.DebugLog($"Found patch point at index {patchIndex} in {targetMethodString}");
-
-            if (patchIndex < 3)  // max index is 3, so we need at least 4 instructions to insert new code
-            {
-                LogUtil.Warning($"Patch index {patchIndex} is too low to insert the new code. Skipping patch.");
-                patchIndex++;
-                continue;
-            }
-
-            // We need to call public static List<ItemStack> ItemCraft.ItemCraftGetAllAvailableItemStacksFromXui(XUi xui) INSTEAD of GetAllItemStacks
-            // locals [1] class [mscorlib]System.Collections.Generic.List`1<class ItemStack>  /* List<ItemStack> allItemStacks */
-
-            codes[patchIndex - 02] = new CodeInstruction(OpCodes.Nop);
-            codes[patchIndex - 01] = new CodeInstruction(OpCodes.Ldarg_1);
-            codes[patchIndex + 00] = new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ItemCraft), nameof(ItemCraft.ItemCraft_GetAllAvailableItemStacksFromXui)));
-            codes[patchIndex + 01] = new CodeInstruction(OpCodes.Stloc_1);
-
-            patchCount++;
-
-            LogUtil.DebugLog($"Applied patch #{patchCount} at index {patchIndex - 2} in {targetMethodString}");
-            patchIndex++; // Move past the newly inserted code
-        }
-
-        if (patchCount > 0)
+        // Create replacement instructions
+        var replacementInstructions = new List<CodeInstruction>
         {
-            LogUtil.Info($"Successfully patched {targetMethodString} in {patchCount} places");
-        }
-        else
-        {
-            LogUtil.Warning($"No patches applied to {targetMethodString}");
-        }
+            new CodeInstruction(OpCodes.Nop),
+            new CodeInstruction(OpCodes.Ldarg_1),
+            new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ItemCraft), nameof(ItemCraft.ItemCraft_GetAllAvailableItemStacksFromXui))),
+            new CodeInstruction(OpCodes.Stloc_1)
+        };
 
-        return codes.AsEnumerable();
+        var patchRequest = new PatchUtil.PatchRequest
+        {
+            OriginalInstructions = instructions.ToList(), // Convert to List for compatibility
+            SearchPattern = searchPattern,
+            ReplacementInstructions = replacementInstructions,
+            TargetMethodName = targetMethodString,
+            ReplacementOffset = -2,   // Start replacement 2 instructions before the match
+            IsInsertMode = false,     // Overwrite existing instructions
+            MaxPatches = 1,
+            MinimumSafetyOffset = 2,  // Ensure we have at least 2 instructions before the match
+            ExtraLogging = false      
+        };
+
+        var response = PatchUtil.ApplyPatches(patchRequest);
+        return response.BestInstructions(patchRequest);
     }
 }
