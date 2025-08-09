1a41eff25c7d9de31710cca84fd5b1766f665889
superguru <superguru@users.noreply.github.com>
2025-08-09 16:50:35 +1200

    Improve world and server status checks

--- BeyondStorage/Scripts/Infrastructure/WorldTools.cs
+++ BeyondStorage/Scripts/Infrastructure/WorldTools.cs
@@ -3,6 +3,24 @@ public static class WorldTools
 {
     public static bool IsServer()
     {
-        return GameManager.IsDedicatedServer || (ConnectionManager.Instance?.IsServer ?? false);
+        //return GameManager.IsDedicatedServer || (ConnectionManager.Instance?.IsServer ?? false);
+        // if (GameManager.Instance == null || !SingletonMonoBehaviour<ConnectionManager>.Instance.IsServer)
+
+        return SingletonMonoBehaviour<ConnectionManager>.Instance.IsServer;
+    }
+
+    public static bool IsClient()
+    {
+        return SingletonMonoBehaviour<ConnectionManager>.Instance.IsClient;
+    }
+
+    public static bool IsSinglePlayer()
+    {
+        return SingletonMonoBehaviour<ConnectionManager>.Instance.IsSinglePlayer;
+    }
+
+    public static bool IsWorldExists()
+    {
+        return GameManager.Instance?.World != null;
     }
 }
