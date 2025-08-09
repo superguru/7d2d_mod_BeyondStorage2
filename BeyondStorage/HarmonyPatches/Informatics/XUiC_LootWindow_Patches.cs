2c690037890e0bd857f19413657529257b2ca617
superguru <superguru@users.noreply.github.com>
2025-08-10 11:45:14 +1200

    Add slot lock change detection to UI components

---
+++ BeyondStorage/HarmonyPatches/Informatics/XUiC_LootWindow_Patches.cs
@@ -0,0 +1,23 @@
+﻿using BeyondStorage.Scripts.Infrastructure;
+using HarmonyLib;
+
+namespace BeyondStorage.HarmonyPatches.UI;
+
+[HarmonyPatch(typeof(XUiC_LootWindow))]
+public class XUiC_LootWindow_Patches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(XUiC_LootWindow.UpdateLockedSlots))]
+#if DEBUG
+    [HarmonyDebug]
+#endif
+    public static void XUiC_LootWindow_UpdateLockedSlots_Postfix(XUiC_ContainerStandardControls _csc)
+    {
+        const string d_MethodName = nameof(XUiC_LootWindow_UpdateLockedSlots_Postfix);
+
+#if DEBUG
+        ModLogger.DebugLog($"{d_MethodName}: Refreshing all windows after updating locked slots.");
+#endif
+        UIRefreshHelper.RefreshAllWindows(d_MethodName, includeViewComponents: true);
+    }
+}
