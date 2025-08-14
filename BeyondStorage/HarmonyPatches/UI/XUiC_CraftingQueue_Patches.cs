4cd9d75f2e68b29727efd4a5c1d372d8fa948a34
superguru <superguru@users.noreply.github.com>
2025-08-15 01:08:46 +1200

    Better logging for game index bug patch

--- BeyondStorage/HarmonyPatches/UI/XUiC_CraftingQueue_Patches.cs
+++ BeyondStorage/HarmonyPatches/UI/XUiC_CraftingQueue_Patches.cs
@@ -4,14 +4,23 @@ using HarmonyLib;
 namespace BeyondStorage.HarmonyPatches.UI;
 
 [HarmonyPatch(typeof(XUiC_CraftingQueue))]
+#if DEBUG
+[HarmonyDebug]
+#endif
 public class XUiCCraftingQueuePatches
 {
     // Fixed an internal bug where crafting queue is not kept in sync with some other UI elements.
     // Still a bug in 2.x - confirmed in 2.0 - 2.2
     [HarmonyPrefix]
     [HarmonyPatch(nameof(XUiC_CraftingQueue.AddRecipeToCraftAtIndex))]
+#if DEBUG
+    [HarmonyDebug]
+#endif
     private static bool XUiC_CraftingQueue_AddRecipeToCraftAtIndex_Prefix(XUiC_CraftingQueue __instance, ref bool __result, int _index, global::Recipe _recipe)
     {
+        //TODO: Remove after debugging tests
+        ModLogger.DebugLog($"XUiC_CraftingQueue.AddRecipeToCraftAtIndex: index: {_index}, recipe: {_recipe?.GetName() ?? "null"}");
+
         var inBounds = _index < __instance.queueItems.Length;
         if (inBounds)
         {
