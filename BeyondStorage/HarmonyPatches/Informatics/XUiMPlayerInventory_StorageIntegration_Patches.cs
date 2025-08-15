d5c66e1adc27fb716a9cc4ff94f7e3577590a8f7
superguru <superguru@users.noreply.github.com>
2025-08-16 04:12:22 +1200

    Add patch to display remaining item counts for temporary messages after repairs and upgrades

---
+++ BeyondStorage/HarmonyPatches/Informatics/XUiMPlayerInventory_StorageIntegration_Patches.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using BeyondStorage.Scripts.Game.Item;
+using BeyondStorage.Scripts.Infrastructure;
+using HarmonyLib;
+
+namespace BeyondStorage.HarmonyPatches.UI;
+
+[HarmonyPatch(typeof(XUiM_PlayerInventory))]
+public static class XUiMPlayerInventory_StorageIntegration_Patches
+{
+#if DEBUG
+    private static long s_callCounter = 0;
+#endif
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(XUiM_PlayerInventory.GetItemCountWithMods))]
+#if DEBUG
+    [HarmonyDebug]
+#endif
+    public static void XUiM_PlayerInventory_GetItemCountWithMods_Postfix(XUiM_PlayerInventory __instance, ItemValue _itemValue, ref int __result)
+    {
+#if DEBUG
+        const string d_MethodName = nameof(XUiM_PlayerInventory_GetItemCountWithMods_Postfix);
+        var callCount = Interlocked.Increment(ref s_callCounter);
+#endif
+        int entityPlayerCount = __result;
+        int storageCount = ItemCommon.ItemCommon_GetAvailableItemCount(_itemValue);
+        __result = entityPlayerCount + storageCount;
+#if DEBUG
+        ModLogger.DebugLog($"{d_MethodName} [{callCount}]: item: {_itemValue.ItemClass.Name}; result {__result} = entityPlayerCount: {entityPlayerCount} + storageCount: {storageCount}");
+#endif
+    }
+}
