687117108084ae8332db0dba6fb766b20cc23816
superguru <superguru@users.noreply.github.com>
2025-08-17 03:52:02 +1200

    Add lockpicking from storage, which is very useful for Drones

---
+++ BeyondStorage/HarmonyPatches/Functions/BlockSecureLoot_Lockpick_Patches.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using BeyondStorage.Scripts.Game.Item;
+using BeyondStorage.Scripts.Harmony;
+using HarmonyLib;
+
+namespace BeyondStorage.HarmonyPatches.Functions;
+
+[HarmonyPatch(typeof(BlockSecureLoot))]
+public static class BlockSecureLoot_Lockpick_Patches
+{
+    [HarmonyTranspiler]
+    [HarmonyPatch(nameof(BlockSecureLoot.OnBlockActivated), [typeof(string), typeof(WorldBase), typeof(int), typeof(Vector3i), typeof(BlockValue), typeof(EntityPlayerLocal)])]
+#if DEBUG
+    [HarmonyDebug]
+#endif
+    private static IEnumerable<CodeInstruction> BlockSecureLoot_OnBlockActivated_Patch(IEnumerable<CodeInstruction> originalInstructions)
+    {
+        var targetMethodString = $"{typeof(BlockSecureLoot)}.{nameof(BlockSecureLoot.OnBlockActivated)}";
+
+        // Create search pattern for GetItemStacksForFilter method call
+        var searchPattern = new List<CodeInstruction>
+        {
+            new CodeInstruction(OpCodes.Ldloc_S, 5),  // itemValue
+            new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(XUiM_PlayerInventory), nameof(XUiM_PlayerInventory.GetItemCount), [typeof(ItemValue)])),
+        };
+
+        // Create replacement instructions
+        var replacementInstructions = new List<CodeInstruction>
+        {
+            new CodeInstruction(OpCodes.Ldloc_S, 5),  // itemValue
+            new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ItemCommon), nameof(ItemCommon.ItemCommon_GetStorageItemCount))),
+            new CodeInstruction(OpCodes.Add),
+        };
+
+        var request = new ILPatchEngine.PatchRequest
+        {
+            OriginalInstructions = [.. originalInstructions],
+            SearchPattern = searchPattern,
+            ReplacementInstructions = replacementInstructions,
+            TargetMethodName = targetMethodString,
+            ReplacementOffset = 2,
+            IsInsertMode = true,
+            MaxPatches = 1,
+            MinimumSafetyOffset = 2,
+            ExtraLogging = false,
+        };
+
+        var response = ILPatchEngine.ApplyPatches(request);
+        return response.BestInstructions(request);
+    }
+}
