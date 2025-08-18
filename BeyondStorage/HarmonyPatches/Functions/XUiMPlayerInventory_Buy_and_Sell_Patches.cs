c5533f761c67b2162f2720c3664431914351d9b3
superguru <superguru@users.noreply.github.com>
2025-08-19 00:11:41 +1200

    Update XUiMPlayerInventory_Buy_and_Sell_Patches.cs

--- BeyondStorage/HarmonyPatches/Functions/XUiMPlayerInventory_Buy_and_Sell_Patches.cs
+++ BeyondStorage/HarmonyPatches/Functions/XUiMPlayerInventory_Buy_and_Sell_Patches.cs
@@ -6,22 +6,22 @@ namespace BeyondStorage.HarmonyPatches.Functions;
 [HarmonyPatch(typeof(XUiM_PlayerInventory))]
 public static class XUiMPlayerInventory_Buy_and_Sell_Patches
 {
-    //    [HarmonyPrefix]
-    //    [HarmonyPatch(nameof(XUiM_PlayerInventory.CanSwapItems), [typeof(ItemStack), typeof(ItemStack), typeof(int)])]
-    //#if DEBUG
-    //    [HarmonyDebug]
-    //#endif
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(XUiM_PlayerInventory.CanSwapItems), [typeof(ItemStack), typeof(ItemStack), typeof(int)])]
+#if DEBUG
+    [HarmonyDebug]
+#endif
     private static bool XUiM_PlayerInventory_CanSwapItems_Prefix(XUiM_PlayerInventory __instance, ItemStack _removedStack, ItemStack _addedStack, int _slotNumber, ref bool __result)
     {
         __result = PurchasingCommon.CanSwapItems(__instance, _removedStack, _addedStack, _slotNumber);
         return false; // Skip original method
     }
 
-    //    [HarmonyPostfix]
-    //    [HarmonyPatch(nameof(XUiM_PlayerInventory.CountAvailableSpaceForItem), [typeof(ItemValue), typeof(bool)])]
-    //#if DEBUG
-    //    [HarmonyDebug]
-    //#endif
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(XUiM_PlayerInventory.CountAvailableSpaceForItem), [typeof(ItemValue), typeof(bool)])]
+#if DEBUG
+    [HarmonyDebug]
+#endif
     private static void XUiM_PlayerInventory_CountAvailableSpaceForItem_Postfix(XUiM_PlayerInventory __instance, ItemValue itemValue, bool limitToOneStack, ref int __result)
     {
         //const string d_MethodName = nameof(XUiM_PlayerInventory_CountAvailableSpaceForItem_Postfix);
