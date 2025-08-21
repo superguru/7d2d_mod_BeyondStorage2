b03f6863fa3ce8b3a4f4e949f64c0d37b5cfbc9c
superguru <superguru@users.noreply.github.com>
2025-08-21 23:31:21 +1200

    Create Stack_MoveAll_Patch.cs

---
+++ BeyondStorage/HarmonyPatches/Informatics/Stack_MoveAll_Patch.cs
@@ -0,0 +1,48 @@
+﻿using BeyondStorage.Scripts.Data;
+using BeyondStorage.Scripts.UI;
+using HarmonyLib;
+
+namespace BeyondStorage.HarmonyPatches.Informatics;
+
+[HarmonyPatch(typeof(XUiC_ContainerStandardControls))]
+public class Stack_MoveAll_Patch
+{
+    private static bool s_isMovingAll = false;
+    private static readonly object s_lockObject = new();
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(XUiC_ContainerStandardControls.MoveAll))]
+#if DEBUG
+    [HarmonyDebug]
+#endif
+    private static void Handle_MoveAll_Event_Prefix(XUiC_ContainerStandardControls __instance)
+    {
+        lock (s_lockObject)
+        {
+            s_isMovingAll = true;
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(XUiC_ContainerStandardControls.MoveAll))]
+#if DEBUG
+    [HarmonyDebug]
+#endif
+    public static void Handle_MoveAll_Event_Postfix(XUiC_ContainerStandardControls __instance)
+    {
+        lock (s_lockObject)
+        {
+            s_isMovingAll = false;
+        }
+
+        UIRefreshHelper.LogAndRefreshUI(StackOps.MoveAll_Operation, CurrencyCache.GetEmptyCurrencyStack(), 0);
+    }
+
+    public static bool IsMovingAll()
+    {
+        lock (s_lockObject)
+        {
+            return s_isMovingAll;
+        }
+    }
+}
