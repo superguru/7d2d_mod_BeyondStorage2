32e20559eb2e08f55f85354c657348b4bcc66e58
superguru <superguru@users.noreply.github.com>
2025-08-23 22:50:16 +1200

    Add GetItemCount diagnostic patch assistance method

--- BeyondStorage/HarmonyPatches/Informatics/XUiMPlayerInventory_StorageIntegration_Patches.cs
+++ BeyondStorage/HarmonyPatches/Informatics/XUiMPlayerInventory_StorageIntegration_Patches.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading;
+using BeyondStorage.Scripts.Data;
 using BeyondStorage.Scripts.Game.Item;
+using BeyondStorage.Scripts.Infrastructure;
 using HarmonyLib;
 
 namespace BeyondStorage.HarmonyPatches.Informatics;
@@ -27,6 +29,44 @@ internal static class XUiMPlayerInventory_StorageIntegration_Patches
         __result = entityPlayerCount + storageCount;
 #if DEBUG
         //ModLogger.DebugLog($"{d_MethodName} [{callCount}]: item: {_itemValue.ItemClass.Name}; result {__result} = entityPlayerCount: {entityPlayerCount} + storageCount: {storageCount}");
+#endif
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(XUiM_PlayerInventory.GetItemCount), [typeof(int)])]
+#if DEBUG
+    [HarmonyDebug]
+#endif
+    private static void XUiM_PlayerInventory_GetItemCount_Int_Postfix(XUiM_PlayerInventory __instance, int _itemId, ref int __result)
+    {
+#if DEBUG
+        const string d_MethodName = nameof(XUiM_PlayerInventory_GetItemCount_Int_Postfix);
+#endif
+        var entityPlayerCount = __result;
+        //var storageCount = ItemCommon.ItemCommon_GetStorageItemCount(_itemId);
+        var itemName = ItemX.NameOf(_itemId);
+
+#if DEBUG
+        ModLogger.DebugLog($"{d_MethodName}: itemId: {_itemId} ({itemName}); result {__result} = entityPlayerCount: {entityPlayerCount} + storageCount: unknown (skipped)");
+#endif
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(XUiM_PlayerInventory.GetItemCount), [typeof(int)])]
+#if DEBUG
+    [HarmonyDebug]
+#endif
+    private static void XUiM_PlayerInventory_GetItemCount_ItemValue_Postfix(XUiM_PlayerInventory __instance, ItemValue _itemValue, ref int __result)
+    {
+#if DEBUG
+        const string d_MethodName = nameof(XUiM_PlayerInventory_GetItemCount_ItemValue_Postfix);
+#endif
+        var entityPlayerCount = __result;
+        //var storageCount = ItemCommon.ItemCommon_GetStorageItemCount(_itemId);
+        var itemName = ItemX.NameOf(_itemValue);
+
+#if DEBUG
+        ModLogger.DebugLog($"{d_MethodName}: ({itemName}); result {__result} = entityPlayerCount: {entityPlayerCount} + storageCount: unknown (skipped)");
 #endif
     }
 }
