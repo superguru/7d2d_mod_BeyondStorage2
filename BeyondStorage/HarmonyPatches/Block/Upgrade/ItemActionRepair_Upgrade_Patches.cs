b36e0623a1a1aca391ae0401b9b06a1b1a51e53c
superguru <superguru@users.noreply.github.com>
2025-08-24 22:07:59 +1200

    Switch to a common item removal method for block upgrade

--- BeyondStorage/HarmonyPatches/Block/Upgrade/ItemActionRepair_Upgrade_Patches.cs
+++ BeyondStorage/HarmonyPatches/Block/Upgrade/ItemActionRepair_Upgrade_Patches.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using BeyondStorage.Scripts.Game.Block;
-using BeyondStorage.Scripts.Infrastructure;
+﻿using BeyondStorage.Scripts.Game.Item;
 using HarmonyLib;
 
 namespace BeyondStorage.HarmonyPatches.Block;
@@ -12,128 +7,88 @@ namespace BeyondStorage.HarmonyPatches.Block;
 internal static class ItemActionRepairUpgradePatches
 {
     // Used For:
-    //          Block Upgrade (Check for enough items)
-    [HarmonyTranspiler]
-    [HarmonyPatch(nameof(ItemActionRepair.CanRemoveRequiredResource))]
+    //          Block Upgrade (Check for enough items) Post Patch
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(ItemActionRepair.CanRemoveRequiredResource), [typeof(ItemInventoryData), typeof(BlockValue)])]
 #if DEBUG
     [HarmonyDebug]
 #endif
-    private static IEnumerable<CodeInstruction> ItemActionRepair_CanRemoveRequiredResource_Patch(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    private static void ItemActionRepair_CanRemoveRequiredResource_Postfix(ItemActionRepair __instance, ItemInventoryData data, BlockValue blockValue, ref bool __result)
     {
-        var targetMethodString = $"{typeof(ItemActionRepair)}.{nameof(ItemActionRepair.CanRemoveRequiredResource)}";
-        ModLogger.Info($"Transpiling {targetMethodString}");
-
-        var codes = instructions.ToList();
-
-        var targetOpCode = OpCodes.Callvirt.Name;
-        var targetType = typeof(Bag);
-        var targetMethod = nameof(Bag.GetItemCount);
-
-        var targetIndex = -1;
-
-        ModLogger.Info($"Looking for: {targetOpCode} for {targetType.Name} method {targetMethod}");
-
-        for (var i = 0; i < codes.Count; i++)
+        // If player already has enough items, no need to check storage
+        if (__result)
         {
-            //ModLogger.Info($"opcode {i} is {codes[i].opcode}");
-            if (codes[i].opcode.Name.Equals(targetOpCode))
-            {
-                // Bag.GetItemCount is overloaded. Can be more acccurate with the method signature, but this is good enough
-                MethodInfo methodInfo = (MethodInfo)codes[i].operand;
-                if ((methodInfo.DeclaringType == targetType) && methodInfo.Name.Equals(targetMethod))
-                {
-                    //ModLogger.Info($"targetOpCode for {methodInfo.DeclaringType.Name} method {methodInfo.Name} means found");
-
-                    targetIndex = i;
-                    break;
-                }
-            }
+            return;
         }
 
-        if (targetIndex > -1)
+        // Get the upgrade item for this block
+        string upgradeItemName = __instance.GetUpgradeItemName(blockValue.Block);
+        if (string.IsNullOrEmpty(upgradeItemName))
         {
-            ModLogger.DebugLog("Adding method to count items from all storages");
-
-            var newLabel = generator.DefineLabel();
-            // ldloc.s  _itemValue [newLabel]
-            var newLabelDestCi = new CodeInstruction(codes[targetIndex - 4].opcode, codes[targetIndex - 4].operand);
-            newLabelDestCi.labels.Add(newLabel);
-            List<CodeInstruction> newCode = [
-                // blt.s    [newLabel]
-                new CodeInstruction(OpCodes.Blt_S, newLabel),
-                // ldc.i4.1
-                new CodeInstruction(OpCodes.Ldc_I4_1),
-                // ret
-                new CodeInstruction(OpCodes.Ret),
-                // ldloc.s  _itemValue [newLabel]
-                newLabelDestCi,
-                // BlockUpgrade.BlockUpgradeGetItemCount(_itemValue)
-                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(BlockUpgrade), nameof(BlockUpgrade.BlockUpgradeGetItemCount))),
-                // Moves result onto stack
-                new CodeInstruction(OpCodes.Ldloc_3)
-            ];
-            codes.InsertRange(targetIndex + 2, newCode);
-            // == END 'Proper' Code ==
-
-            ModLogger.Info($"Successfully patched {targetMethodString}");
-        }
-        else
-        {
-            ModLogger.Error($"Failed to patch {targetMethodString}");
+            return;
         }
 
-        return codes.AsEnumerable();
+        ItemValue upgradeItemValue = ItemClass.GetItem(upgradeItemName);
+
+        // Check if storage has the required upgrade items
+        __result = ItemCommon.HasItemInStorage(upgradeItemValue);
     }
 
-    // Used For:
-    //          Block Upgrade (ClearStacksForFilter items)
-    [HarmonyTranspiler]
-    [HarmonyPatch(nameof(ItemActionRepair.RemoveRequiredResource))]
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(ItemActionRepair.RemoveRequiredResource), [typeof(ItemInventoryData), typeof(BlockValue)])]
 #if DEBUG
     [HarmonyDebug]
 #endif
-    private static IEnumerable<CodeInstruction> ItemActionRepair_RemoveRequiredResource_Patch(
-        IEnumerable<CodeInstruction> instructions)
+    private static bool ItemActionRepair_RemoveRequiredResource_Prefix(ItemActionRepair __instance, ItemInventoryData data, BlockValue blockValue, ref bool __result)
     {
-        var targetMethodString = $"{typeof(ItemActionRepair)}.{nameof(ItemActionRepair.RemoveRequiredResource)}";
-        ModLogger.Info($"Transpiling {targetMethodString}");
-        var codes = new List<CodeInstruction>(instructions);
-        var found = false;
-        for (var i = 0; i < codes.Count; i++)
+        // Replicate original validation logic
+        if (!__instance.CanRemoveRequiredResource(data, blockValue))
         {
-            // if (data.holdingEntity.bag.DecItem(_itemValue, result) != result)
-            if (codes[i].opcode != OpCodes.Callvirt ||
-                (MethodInfo)codes[i].operand != AccessTools.Method(typeof(Bag), nameof(Bag.DecItem)))
+            __result = false;
+            return false; // Skip original method
+        }
+
+        global::Block block = blockValue.Block;
+        ItemValue itemValue = ItemClass.GetItem(__instance.GetUpgradeItemName(block));
+
+        // Get required count from block properties
+        if (!int.TryParse(block.Properties.Values[global::Block.PropUpgradeBlockClassItemCount], out var requiredCount))
+        {
+            __result = false;
+            return false; // Skip original method
+        }
+
+        // Get player entity from the inventory data
+        EntityPlayer entityPlayer = data.holdingEntity as EntityPlayer;
+        if (entityPlayer == null)
+        {
+            __result = false;
+            return false; // Skip original method
+        }
+
+        // Use sequential removal: Bag → Toolbelt → Storage (enhanced logic)
+        // Original game uses Toolbelt → Bag, but we use Bag → Toolbelt for consistency
+        int totalRemoved = ItemCommon.RemoveItemsSequential(
+            entityPlayer.bag,
+            entityPlayer.inventory,
+            itemValue,
+            requiredCount
+        );
+
+        // Success if we removed the exact amount needed (original logic)
+        bool success = totalRemoved == requiredCount;
+
+        if (success)
+        {
+            // Update UI to show item consumption (original logic)
+            EntityPlayerLocal entityPlayerLocal = data.holdingEntity as EntityPlayerLocal;
+            if (entityPlayerLocal != null && requiredCount != 0)
             {
-                continue;
+                entityPlayerLocal.AddUIHarvestingItem(new ItemStack(itemValue, -requiredCount));
             }
-
-            ModLogger.DebugLog("Adding method to remove items from all storages");
-
-            found = true;
-            List<CodeInstruction> newCode = [
-                // _itemValue
-                new CodeInstruction(OpCodes.Ldloc_1),
-                // result
-                new CodeInstruction(OpCodes.Ldloc_2),
-                // BlockUpgrade.BlockUpgradeRemoveRemaining(bag.DecItem(...), _itemValue, result)
-                new CodeInstruction(OpCodes.Call,
-                    AccessTools.Method(typeof(BlockUpgrade), nameof(BlockUpgrade.BlockUpgradeRemoveRemaining)))
-            ];
-            codes.InsertRange(i + 1, newCode);
-
-            break;
         }
 
-        if (!found)
-        {
-            ModLogger.Error($"Failed to patch {targetMethodString}");
-        }
-        else
-        {
-            ModLogger.Info($"Successfully patched {targetMethodString}");
-        }
-
-        return codes.AsEnumerable();
+        __result = success;
+        return false; // Skip original method
     }
 }
