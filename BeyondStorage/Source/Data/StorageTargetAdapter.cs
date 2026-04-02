6cbaf79fe21453ba09740e3c1071cccd91c3d4f3
superguru <superguru@users.noreply.github.com>
2026-04-03 04:20:01 +1300

    Create StorageTargetAdapter.cs

---
+++ BeyondStorage/Source/Data/StorageTargetAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondStorage.Scripts.Data;
+
+internal class StorageTargetAdapter<T> where T : class
+{
+    private readonly StorageSourceAdapter<T> _source;
+    private readonly float _distance;
+
+    public StorageTargetAdapter(StorageSourceAdapter<T> source, float distance)
+    {
+        _source = source;
+        _distance = distance;   
+    }
+
+    public StorageSourceAdapter<T> Source => _source;
+    public float Distance => _distance;
+}
