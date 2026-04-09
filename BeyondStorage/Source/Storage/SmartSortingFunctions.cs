7fdee30b665498d1b3a1d43771bceb0f298106d0
superguru <superguru@users.noreply.github.com>
2026-04-09 16:06:38 +1200

    Remove some obsolete debug code

--- BeyondStorage/Source/Storage/SmartSortingFunctions.cs
+++ BeyondStorage/Source/Storage/SmartSortingFunctions.cs
@@ -9,70 +9,6 @@ namespace BeyondStorage.Scripts.Storage;
 
 public class SmartSortingFunctions
 {
-#if DEBUG
-    private static void LogSourceItems(string d_MethodName, ItemStack[] sourceItems)
-    {
-        if (sourceItems == null || sourceItems.Length == 0)
-        {
-            ModLogger.DebugLog($"{d_MethodName}: No source items found.");
-            return;
-        }
-
-        ModLogger.DebugLog($"{d_MethodName}: Source items:");
-        int maxItems = sourceItems.Length;
-        for (int i = 0; i < maxItems; i++)
-        {
-            var itemStack = sourceItems[i];
-            if (itemStack != null)
-            {
-                ModLogger.DebugLog($"  Slot {i + 1}/{maxItems}: {itemStack.count}x {ItemX.NameOf(itemStack)}");
-            }
-            else
-            {
-                ModLogger.DebugLog($"  Slot {i + 1}/{maxItems}: Empty");
-            }
-        }
-    }
-#endif
-
-#if DEBUG
-    private static void LogTargetItems(string d_MethodName, IReadOnlyList<StorageTargetAdapter<ITileEntityLootable>> targets)
-    {
-        if (targets == null || targets.Count == 0)
-        {
-            ModLogger.DebugLog($"{d_MethodName}: No target containers found.");
-            return;
-        }
-
-        ModLogger.DebugLog($"{d_MethodName}: Target containers:");
-        int maxContainers = targets.Count;
-        for (int i = 0; i < maxContainers; i++)
-        {
-            var target = targets[i];
-
-            string containerName = target.GetTargetName();
-
-            var items = target.GetAllSlotItemStacks();
-            int maxItems = items.Length;
-
-            ModLogger.DebugLog($"  Container {i + 1}/{maxContainers}: {containerName} ({maxItems} slots) Distance: {targets[i].Distance:0.###}");
-
-            for (int j = 0; j < maxItems; j++)
-            {
-                var itemStack = items[j];
-                if (itemStack == null || itemStack.count <= 0)
-                {
-                    ModLogger.DebugLog($"    Slot {j + 1}/{maxItems}: Empty");
-                }
-                else
-                {
-                    ModLogger.DebugLog($"    Slot {j + 1}/{maxItems}: {itemStack.count}x {ItemX.NameOf(itemStack)}");
-                }
-            }
-        }
-    }
-#endif
-
     public static void SmartCollectorPush()
     {
         const string d_MethodName = nameof(SmartCollectorPush);
@@ -131,9 +67,6 @@ public class SmartSortingFunctions
 
         var source = StorageSourceAdapterFactory.CreatePlayerLootableSourceAdapter(context, context.Player);
         var targets = context.GetClosestTargetContainers();
-#if DEBUG
-        //LogTargetItems(d_MethodName, targets);
-#endif
 
         PerformSmartPush(context, source, targets);
     }
@@ -217,9 +150,6 @@ public class SmartSortingFunctions
         const string d_MethodName = nameof(PushSourceItemsToTarget);
 
         var sourceSlots = source.GetPushableItemStacks();
-#if DEBUG
-        //LogSourceItems(d_MethodName, sourceSlots);
-#endif
 
         for (int i = 0; i < sourceSlots.Length; i++)
         {
