bb457771a1292b5a8aa85278f51496169f4c6e4f
superguru <superguru@users.noreply.github.com>
2026-04-13 18:25:44 +1200

    Add ListExtensions class

---
+++ BeyondStorage/Source/Data/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace BeyondStorage.Source.Data;
+
+public static class ListExtensions
+{
+    public static int IndexOfReference<T>(this List<T> list, T target) where T : class
+    {
+        int count = list.Count;
+        for (int i = 0; i < count; i++)
+        {
+            if (object.ReferenceEquals(list[i], target))
+                return i;
+        }
+
+        return -1;
+    }
+}
