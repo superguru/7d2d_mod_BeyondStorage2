e48289fbf2dc6a918642c9c90d5295e4dfb90d5a
superguru <superguru@users.noreply.github.com>
2026-04-14 00:53:07 +1200

    Update List Extensions with null safety check

--- BeyondStorage/Source/Data/ListExtensions.cs
+++ BeyondStorage/Source/Data/ListExtensions.cs
@@ -6,10 +6,15 @@ public static class ListExtensions
 {
     public static int IndexOfReference<T>(this List<T> list, T target) where T : class
     {
+        if (list == null || target == null)
+        {
+            return -1;
+        }
+
         int count = list.Count;
         for (int i = 0; i < count; i++)
         {
-            if (object.ReferenceEquals(list[i], target))
+            if (ReferenceEquals(list[i], target))
                 return i;
         }
 
