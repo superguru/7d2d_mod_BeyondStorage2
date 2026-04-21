075674c4460869bfe15f57a528f4e58f1c71f256
superguru <superguru@users.noreply.github.com>
2026-04-21 12:59:09 +1200

    Release v2.6.8

--- BeyondStorage/Properties/AssemblyInfo.cs
+++ BeyondStorage/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.7.*")]
-[assembly: AssemblyFileVersion("2.6.7")]
+[assembly: AssemblyVersion("2.6.8.*")]
+[assembly: AssemblyFileVersion("2.6.8")]
