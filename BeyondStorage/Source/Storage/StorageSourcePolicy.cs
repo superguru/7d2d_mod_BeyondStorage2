e7b428a5c12e6073e0663a5684cec10810e1ecdf
superguru <superguru@users.noreply.github.com>
2026-04-23 02:41:44 +1200

    Added the ability to push to your Drone of Vehicle while you are On Mission (not near any other storages)

--- BeyondStorage/Source/Storage/StorageSourcePolicy.cs
+++ BeyondStorage/Source/Storage/StorageSourcePolicy.cs
@@ -13,6 +13,9 @@ internal static class StorageSourcePolicy
     /// <summary>Gets the allowed source types for smart push operations.</summary>
     internal static AllowedSourcesList SmartPushSources { get; } = BuildSmartPushSources();
 
+    /// <summary>Gets the allowed source types for smart push operations while On Mission.</summary>
+    internal static AllowedSourcesList SmartOnMissionPushSources { get; } = BuildSmartOnMissionPushSources();
+
     /// <summary>Gets the allowed source types for smart loadout pull operations.</summary>
     internal static AllowedSourcesList SmartLoadoutPullSources { get; } = BuildSmartLoadoutPullSources();
 
@@ -21,6 +24,16 @@ internal static class StorageSourcePolicy
         var types = new List<Type>
         {
             typeof(ITileEntityLootable),
+        };
+
+        return new AllowedSourcesList(types);
+    }
+    private static AllowedSourcesList BuildSmartOnMissionPushSources()
+    {
+        var types = new List<Type>
+        {
+            typeof(EntityDrone),
+            typeof(EntityVehicle),
         };
 
         return new AllowedSourcesList(types);
